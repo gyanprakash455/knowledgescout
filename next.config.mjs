5ff3f14dca6c7e94c9d8f3b3b4fcead74ad33b7d
Gyan Prakash Yadav <ygyan470@gmail.com>
2025-10-05 23:12:15 +0530

    added asdasd

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
